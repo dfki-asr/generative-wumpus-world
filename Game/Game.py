5c85fd38bb6c9b412fe5cf522000c20e7af67273
Melvin-chelli <Melvin.Chelli@dfki.de>
2022-01-14 10:33:02 +0100

    Corrected the order of arguments for grid setup

--- Game/Game.py
+++ Game/Game.py
@@ -5,7 +5,7 @@ from Agent.agentObjet import agentobject
 class game():
     def __init__(self, n_wumpus, n_golds, n_pits, n_agents, n_initChrom, dimension):
         agents = []
-        grid = gridSetup(dimension, n_wumpus, n_golds, n_pits)
+        grid = gridSetup(dimension, n_pits, n_golds, n_wumpus)
         for i in range(n_agents):
             temp = agentobject(n_initChrom, grid, dimension)
             agents.append(temp)
