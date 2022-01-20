5c9fc9a28c132ce03f0b4c5caa5cc1129a5df4f3
Melvin-chelli <Melvin.Chelli@dfki.de>
2022-01-20 11:37:59 +0100

    New action mappings

--- Actions/actionMappings.py
+++ Actions/actionMappings.py
@@ -1,24 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  5 12:25:03 2022
-
-@author: tospie
-"""
-
-from Actions import directionMappings
-from random import randint
-
-
-def move(agent, direction = None) :
-    x, y = agent.locatedAt
-    if not direction:
-        agent.locatedAt = x + randint(-1, 1), y + randint(-1, 1)
-    else:
-        agent.locatedAt += directionMappings[direction]
-
-def setToBounds(agent) :
-    agent.locatedAt = agent.locatedAt ## later set Agent to grid borders
-
-mappingTable = {
-
-}
+tab_of_act = {
+            'W': ('move', 'N'),
+            'S': ('move', 'S'),
+            'A': ('move', 'E'),
+            'D': ('move', 'W'),
+            'P': ('pickup'),
+            'w': ('shoot', 'N'),
+            's': ('shoot', 'S'),
+            'a': ('shoot', 'E'),
+            'd': ('shoot', 'W')
+        }
