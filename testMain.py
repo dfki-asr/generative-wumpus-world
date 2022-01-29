b91c939f324516e9c1649d7f484d4b7827f3f0a2
Melvin-chelli <Melvin.Chelli@dfki.de>
2022-01-29 15:13:42 +0100

    now uses run_game() method, to suit the restructured Game.py object

--- testMain.py
+++ testMain.py
@@ -4,7 +4,8 @@ from Agent.agentObjet import agentobject
 
 
 def main():
-    game1 = game(n_wumpus=1, n_golds=1, n_pits=5, n_agents=10, n_initChrom=2, dimension=10)
+    game1 = game(n_wumpus=1, n_golds=1, n_pits=9, n_agents=5, n_initChrom=4, dimension=10)
+    game1.run_game()
 
 
 if __name__ == '__main__':
