54d84f6ca22606e2bbdb7129f39155abfce846b4
Melvin-chelli <Melvin.Chelli@dfki.de>
2022-02-03 14:05:52 +0100

    num of agents passed as a parameter

--- testMain.py
+++ testMain.py
@@ -4,7 +4,7 @@ from Agent.agentObjet import agentobject
 from Game.ga_game import ga_game
 
 def main():
-    game1 = ga_game(50, 0.8, 0.3)
+    game1 = ga_game(5, 5, 0.8, 0.3)
     game1.run()
 
 if __name__ == '__main__':
