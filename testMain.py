2aff38a8afbdbfa81e1d80bfc6abc9c8d4233d03
Melvin-chelli <Melvin.Chelli@dfki.de>
2022-02-04 15:22:24 +0100

    redirecting print statements to a file in logs folder

--- testMain.py
+++ testMain.py
@@ -1,11 +1,25 @@
-from Game.Game import game
-from Environment.gridSetup import gridSetup
-from Agent.agentObjet import agentobject
 from Game.ga_game import ga_game
+import sys
+import os
+from datetime import datetime
+
 
 def main():
-    game1 = ga_game(5, 5, 0.8, 0.3)
+    cwd = os.getcwd()
+    targetPath = os.path.join(cwd, "logs")
+    while not os.path.exists(targetPath):
+        os.mkdir(targetPath)
+    timestr = datetime.now().strftime("%d%m%Y-%H%M%S")
+    targetFile = os.path.join(targetPath, timestr)
+
+    orig_stdout = sys.stdout
+    f = open(targetFile, 'w')
+    sys.stdout = f
+    game1 = ga_game(10, 50, 0.9, 0.05)
     game1.run()
+    sys.stdout = orig_stdout
+    f.close()
+
 
 if __name__ == '__main__':
     main()
