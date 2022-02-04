f2262fbe4d85bb8da2a64f8f47b5acd7b886fe84
Melvin-chelli <Melvin.Chelli@dfki.de>
2022-02-04 15:26:45 +0100

    Changes to prevent creation of empty files when not using the file logging feature

--- testMain.py
+++ testMain.py
@@ -4,22 +4,25 @@ import os
 from datetime import datetime
 
 
-def main():
-    cwd = os.getcwd()
-    targetPath = os.path.join(cwd, "logs")
-    while not os.path.exists(targetPath):
-        os.mkdir(targetPath)
-    timestr = datetime.now().strftime("%d%m%Y-%H%M%S")
-    targetFile = os.path.join(targetPath, timestr)
+def main(print_file):
+    if print_file:
+        cwd = os.getcwd()
+        targetPath = os.path.join(cwd, "logs")
+        while not os.path.exists(targetPath):
+            os.mkdir(targetPath)
+        timestr = datetime.now().strftime("%d%m%Y-%H%M%S")
+        targetFile = os.path.join(targetPath, f'{timestr}.txt')
+        orig_stdout = sys.stdout
+        f = open(targetFile, 'w')
+        sys.stdout = f
 
-    orig_stdout = sys.stdout
-    f = open(targetFile, 'w')
-    sys.stdout = f
     game1 = ga_game(10, 50, 0.9, 0.05)
     game1.run()
-    sys.stdout = orig_stdout
-    f.close()
+
+    if print_file:
+        sys.stdout = orig_stdout
+        f.close()
 
 
 if __name__ == '__main__':
-    main()
+    main(print_file=False)
