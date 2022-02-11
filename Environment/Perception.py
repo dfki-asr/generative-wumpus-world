e73b7dec3aeecf9f408b96736fe957a8c026ca9b
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2022-02-11 10:29:45 +0100

    Added perception object to store perceptions

---
+++ Environment/Perception.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+class Perception() :
+    def __init__ (self,source,phen,lvl,t,dec):
+        self.source =  source
+        self.phen = phen
+        self.t = t
+        self.dec = dec
+        self.lvl = lvl
