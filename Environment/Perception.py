52bf95c2c00c419d0977291898e4c97dc1955a2e
Melvin-chelli <Melvin.Chelli@dfki.de>
2022-02-11 22:23:19 +0100

    added getLvl method to get intensity of safe markers

--- Environment/Perception.py
+++ Environment/Perception.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
 class Perception() :
-    def __init__ (self,source,phen,lvl,t,dec):
-        self.source =  source
+    def __init__ (self, source, phen, lvl, t, now, dec):
+        self.source = source
         self.phen = phen
         self.t = t
         self.dec = dec
         self.lvl = lvl
+        self.now = now
+
+
+    def getLevel(self):
+        self.lvl = self.lvl - self.dec*self.now
