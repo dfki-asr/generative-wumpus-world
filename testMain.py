e8c8e540a11ab65668c09181c3a6702d36e7b75a
Melvin-chelli <Melvin.Chelli@dfki.de>
2022-03-01 14:25:57 +0100

    Number of generations to be run is not a parameter now, since we have stopping criteria

--- testMain.py
+++ testMain.py
@@ -16,7 +16,7 @@ def main(print_file):
         f = open(targetFile, 'w')
         sys.stdout = f
 
-    game1 = ga_game(10,20, 0.8, 0.25)
+    game1 = ga_game(n_agents=20, crossover_rate=0.8, mutation_rate=0.25)
     game1.run()
 
     if print_file:
